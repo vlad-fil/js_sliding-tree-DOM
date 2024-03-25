cbff082cc3f86badeb0b5163988e541a14b5df03
Vladyslav Filonenko <oladik05@gmail.com>
2024-03-25 22:42:37 +0200

    make build

---
+++ index.e0c7fe21.js
@@ -0,0 +1,2 @@
+document.querySelector(".tree").addEventListener("click",function(e){if("tree"!==e.target.className){var t=e.target.children[0];if("none"===t.style.display){t.style.display="block";return}t.style.display="none"}});
+//# sourceMappingURL=index.e0c7fe21.js.map
